7bda35a7677954b581e0e15c0cba674e6c18c48a
Bluesky87 <grzegorz_kasperek@interia.pl>
2018-11-20 11:32:49 +0100

    l2 commint

---
+++ l2/Controllers/FilesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FilesController extends Controller
+{
+    public function show()
+    {
+        return response()->download(storage_path('app/file.png'), 'photoTest');
+    }
+
+    public function create(Request $request)
+    {
+        $path = $request->file('photo')->store('testing');
+
+        return response()->json(['path' => $path], 200);
+    }
+}
